84df8c37223950fb45becf5865011a4028497918
Cybyl J Fine <102087890+cjhanc15@users.noreply.github.com>
2025-05-16 01:38:31 -0400

    update vite.config.ts

--- Fine-Dining/vite.config.ts
+++ Fine-Dining/vite.config.ts
@@ -2,9 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Fine-Dining/FineWare',
+  base: '/Fine-Dining.FineWare',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
 });
